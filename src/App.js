bf8f0e183865aa271437ed3de8b72766abd19260
Misha Milovidov <m.alexeyvich@gmail.com>
2019-01-08 13:42:10 -0700

    (59) - Setup firebase client

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCldzBeQoIXXJ_6n4yK-KfHETtczCHdaOQ',
+      authDomain: 'test-project-b9edd.firebaseapp.com',
+      databaseURL: 'https://test-project-b9edd.firebaseio.com',
+      projectId: 'test-project-b9edd',
+      storageBucket: 'test-project-b9edd.appspot.com',
+      messagingSenderId: '223812986460'
+    });
+  }
+
   render() {
     return (
       <View>
