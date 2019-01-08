1786ec772015b4f4098b1f27ef4defa44524e3e9
Misha Milovidov <m.alexeyvich@gmail.com>
2019-01-08 14:19:44 -0700

    (66) - Make the input pretty

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -2,16 +2,42 @@ import React from 'react';
 import { TextInput, Text, View } from 'react-native';
 
 const Input = ({ label, onChangeText, value }) => {
+  const { inputStyle, labelStyle, containerStyle } = styles;
+
   return (
-    <View>
-      <Text>{label}</Text>
+    <View style={containerStyle}>
+      <Text style={labelStyle}>{label}</Text>
       <TextInput
-        style={{ height: 20, width: 100 }}
+        style={inputStyle}
         onChangeText={onChangeText}
         value={value}
       />
     </View>
   );
+};
+
+const styles = {
+  inputStyle: {
+    color: '#000',
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 18,
+    lineHeight: 23,
+    flex: 2,
+    height: 20,
+    width: 100,
+  },
+  labelStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+    flex: 1,
+  },
+  containerStyle: {
+    height: 40,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
 };
 
 export { Input };
